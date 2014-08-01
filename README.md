bf2b4b7457a17fe1d80bb55024a01db813e04479
Shawn McElroy <shawn@skift.io>
2014-07-31 17:31:14 -0700

    plugin works. committing to github and plugin repo

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
-# Open into Marked
+# Open into LightPaper Markdown Editor
 
-Open *.md file into Markdown Viewer Marked.<br>
-You need to install 'Marked'.<br>
+Open your current *.md files into the Markdown Viewer LightPaper.<br>
+You need to install the app 'LightPaper'.<br>
 Visit this site.<br>
-http://markedapp.com/
+http://clockworkengine.com/lightpaper-mac/
+
+Simply enter in the command <kbd>ctrl</kbd> + alt + M top open the current
+file into
+lightpaper.
+
+All credit should go to the open in marked plugin written by @vexus2.
+This project was cloned from there and simply updated to work with my preferred
+markdown editor.
+
+https://github.com/vexus2/open-in-marked
--- src/com/skiftio/intellij/lightpaper/LightPaper.java
+++ src/com/skiftio/intellij/lightpaper/LightPaper.java
@@ -1,4 +1,4 @@
-package com.vexus2.intellij.marked;
+package com.skiftio.intellij.lightpaper;
 
 import com.intellij.notification.Notification;
 import com.intellij.notification.NotificationType;
@@ -11,24 +11,28 @@ import com.intellij.openapi.vfs.VirtualFileManager;
 
 import java.io.IOException;
 
-public class Marked extends AnAction {
-  private static final String MARKED_APP_PATH = "/Applications/Marked.app";
+public class LightPaper extends AnAction {
+  private static final String LIGHTPAPER_APP_PATH = "/Applications/LightPaper.app";
 
   public void update(AnActionEvent e) {
     e.getPresentation().setEnabled(PlatformDataKeys.EDITOR.getData(e.getDataContext()) != null);
   }
 
   public void actionPerformed(AnActionEvent e) {
-    VirtualFile current_file = PlatformDataKeys.VIRTUAL_FILE.getData(e.getDataContext());
+    VirtualFile current_file = (VirtualFile) PlatformDataKeys.VIRTUAL_FILE.getData(e.getDataContext());
 
-    VirtualFile app = VirtualFileManager.getInstance().findFileByUrl("file://" + MARKED_APP_PATH);
+    VirtualFile app = VirtualFileManager.getInstance().findFileByUrl("file://" + LIGHTPAPER_APP_PATH);
 
     if (app == null) {
-      Notifications.Bus.notify(new Notification("Marked", "Information", "'Marked' is not installed.", NotificationType.INFORMATION));
+      Notifications.Bus.notify(new Notification("LightPaper", "Information", "'LightPaper' is not installed.", NotificationType.INFORMATION));
       return;
     }
     if (current_file == null || !current_file.getPath().toString().matches(".*?\\.md$")) {
-      Notifications.Bus.notify(new Notification("Marked", "Information", "This is not Markdown file.", NotificationType.INFORMATION));
+      Notifications.Bus.notify(new Notification("LightPaper", "Information", "This is not " +
+              "" +
+              "" +
+              "" +
+              "Markdown file.", NotificationType.INFORMATION));
       return;
     }
 
@@ -36,9 +40,9 @@ public class Marked extends AnAction {
 
     try {
       String file_path = current_file.getPath().replace(" ", "\\ ");
-      rt.exec("open -a " + MARKED_APP_PATH + " " + file_path);
+      rt.exec("open -a " + LIGHTPAPER_APP_PATH + " " + file_path);
     } catch (IOException e1) {
-      Notifications.Bus.notify(new Notification("Marked", "Error", e1.getMessage(), NotificationType.ERROR));
+      Notifications.Bus.notify(new Notification("LightPaper", "Error", e1.getMessage(), NotificationType.ERROR));
     }
 
   }
